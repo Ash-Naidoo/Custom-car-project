76c8faf2b5735fed154789cccbd07b1b176d43ea
Ash <Ashrafnaidoo86@gmail.comgit config --global user.email you@example.com>
2021-11-12 17:05:22 +0200

    commit

---
+++ New app/js/main.js
@@ -0,0 +1,68 @@
+
+function btn1(){
+
+    const Text1 =(document.getElementById("card-text1"))
+
+    Text1.innerHTML =
+    "We have the latest technology and perfomance parts to boost your Horsepower";
+
+}
+
+function btn2(){
+
+    const Text2 =(document.getElementById("card-text2"))
+
+    Text2.innerHTML =
+    "Best upgrade parts for all support to your modifications";
+
+}
+
+
+function btn3(){
+
+    const Text3 =(document.getElementById("card-text3"))
+
+    Text3.innerHTML =
+    "Latest accessories for the perfect styling";
+
+}
+
+ 
+function Validate(){
+
+        var firstname = document.getElementById("First name").value;
+        var lastname = document.getElementById("Last name").value;
+        var password = document.getElementById("Password").value;
+
+        if ( firstname == "ash" && lastname== "naidoo" && password == "1234"){
+
+            const entertext =(document.getElementById("top sentence"))
+
+            entertext.innerHTML ="Hello you are Welcome!!";
+            
+
+       
+       
+
+}   
+    
+        
+
+
+}
+
+
+
+    
+
+            
+ 
+
+
+ 
+  
+
+
+
+    
+   
